97cf2a88123726a38068bde9dd04bf08a4588fe8
An Tran Van <86640630+antrvan746@users.noreply.github.com>
2023-06-16 23:04:21 +0700

    Minor update

--- ride-hailing-webapp/src/pages/index.tsx
+++ ride-hailing-webapp/src/pages/index.tsx
@@ -5,34 +5,48 @@ import React from "react";
 import GrabBackground from "../../assets/grab_background.jpg";
 import Navigator from "@/components/Navigator";
 import Head from "next/head";
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledPageContainer = styled.div``;
+
+const StyledDriverRegisterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+`;
+
+const StyledTitle = styled.span`
+	
+`;
 
 const Home: NextPage = () => {
-	return (
-		<div>
-			<Head>
-				<title>Mai Grab</title>
-				<meta name="description" content="Created by NextJs" />
-			</Head>
+  return (
+    <StyledPageContainer>
+      <Head>
+        <title>Mai Grab</title>
+        <meta name="description" content="Created by NextJs" />
+      </Head>
 
-			<Header />
-			<Image
-				src={GrabBackground}
-				alt="Grab background"
-				sizes="100vw"
-				style={{
-					width: "100%",
-					height: "auto",
-				}}
-			/>
+      <Header />
+      <Image
+        src={GrabBackground}
+        alt="Grab background"
+        sizes="100vw"
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+      />
 
-			<Navigator text="Ho tro khach hang" backgroundColor="#0a7362" />
-			<Navigator text="Tai ung dung" backgroundColor="#5ab38b" />
+      <Navigator text="Ho tro khach hang" backgroundColor="#0a7362" />
+      <Navigator text="Tai ung dung" backgroundColor="#5ab38b" />
 
-			
-
-
-		</div>
-	);
+      <StyledDriverRegisterWrapper>
+        <span> Đăng ký trở thành cộng tác viên với chúng tôi </span>
+      </StyledDriverRegisterWrapper>
+    </StyledPageContainer>
+  );
 };
 
 export default Home;
