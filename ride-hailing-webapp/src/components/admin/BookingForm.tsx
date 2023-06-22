eb12b49ef6afcbf11f41fc95e2180aca4f2b33f3
antrvan746 <86640630+antrvan746@users.noreply.github.com>
2023-06-22 18:03:51 +0700

    Minor change

--- ride-hailing-webapp/src/components/admin/BookingForm.tsx
+++ ride-hailing-webapp/src/components/admin/BookingForm.tsx
@@ -4,10 +4,19 @@ import {
 	Box,
 	Container,
 	FormControl,
+	FormControlLabel,
 	FormLabel,
+	InputLabel,
+	Radio,
 	RadioGroup,
 	TextField,
+	Select,
+	MenuItem,
+	SelectChangeEvent,
 } from "@mui/material";
+import MotorcycleIcon from "../../../assets/motorcycle.png";
+import SedanIcon from "../../../assets/sedan.png";
+import ShuttleBusIcon from "../../../assets/shuttlebus.png";
 import Image from "next/image";
 import PlaceIcon from "@mui/icons-material/Place";
 
@@ -24,6 +33,11 @@ const StyledPlaceInput = styled.div`
 `;
 
 const BookingForm = () => {
+	const [age, setAge] = React.useState("");
+
+	const handleChange = (event: SelectChangeEvent) => {
+		setAge(event.target.value);
+	};
 	return (
 		<StyledContainer>
 			<FormControl style={{ width: "95%" }}>
@@ -60,10 +74,82 @@ const BookingForm = () => {
 					/>
 				</StyledPlaceInput>
 
-				<FormControl>
-					<FormLabel> Loai xe can dat </FormLabel>
-					<RadioGroup></RadioGroup>
-				</FormControl>
+				<FormLabel
+					style={{
+						paddingLeft: "1rem",
+						marginTop: "2rem",
+					}}
+				>
+					{" "}
+					Loại xe cần đặt{" "}
+				</FormLabel>
+				<RadioGroup
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						paddingLeft: "2rem",
+					}}
+				>
+					<FormControlLabel
+						value="motorcycle"
+						control={<Radio />}
+						label={
+							<>
+								<Image
+									src={MotorcycleIcon}
+									alt="Motorcycle icon"
+									height={50}
+								/>
+							</>
+						}
+					/>
+					<FormControlLabel
+						value="4seat"
+						control={<Radio />}
+						label={
+							<>
+								<Image
+									src={SedanIcon}
+									alt="Sedan icon"
+									height={50}
+								/>
+							</>
+						}
+					/>
+					<FormControlLabel
+						value="7seat"
+						control={<Radio />}
+						label={
+							<>
+								<Image
+									src={SedanIcon}
+									alt="Sedan icon"
+									height={50}
+								/>
+							</>
+						}
+					/>
+				</RadioGroup>
+
+				<div>
+					<InputLabel id="">Age</InputLabel>
+					<Select
+						labelId="demo-select-small-label"
+						id="demo-select-small"
+						value={age}
+						label="Age"
+						onChange={handleChange}
+						style={{ marginTop: "2rem" }}
+					>
+						<MenuItem value="">
+							<em>None</em>
+						</MenuItem>
+						<MenuItem value={10}>VISA</MenuItem>
+						<MenuItem value={20}>MOMO</MenuItem>
+						<MenuItem value={30}>BANKING</MenuItem>
+						<MenuItem value={40}>CASH</MenuItem>
+					</Select>
+				</div>
 			</FormControl>
 		</StyledContainer>
 	);
