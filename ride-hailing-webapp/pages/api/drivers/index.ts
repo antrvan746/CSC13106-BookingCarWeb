8c8df4f0e6448438047079dee5e95904f75e7a16
Phu Han <phuhanld@gmail.com>
2023-07-10 18:50:27 +0700

    API: create & get all drivers

---
+++ ride-hailing-webapp/pages/api/drivers/index.ts
@@ -0,0 +1,43 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { prismaClient } from "../../../libs/prisma";
+import z from "zod";
+
+const driverSchema = z.object({
+  phone: z.string().max(11),
+  password: z.string().max(30),
+  name: z.string(),
+  rating: z.number().default(5),
+});
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    // Get all drivers
+    try {
+      const drivers = await prismaClient.driver.findMany();
+      res.status(200).json(drivers);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Something went wrong' });
+    }
+  } else if (req.method === 'POST') {
+    // Create a new driver
+    try {
+      const { phone, password, name, rating } = driverSchema.parse(req.body);
+      const driver = await prismaClient.driver.create({
+        data: {
+          phone,
+          password,
+          name,
+          rating,
+        },
+      });
+      res.status(201).json(driver);
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ error: 'Invalid request payload' });
+    }
+  } else {
+    res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+    res.status(405).json({ error: `Method ${req.method} Not Allowed` });
+  }
+};
