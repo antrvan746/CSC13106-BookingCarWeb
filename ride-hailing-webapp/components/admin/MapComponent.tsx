2be49dd8ce3ec9e91032e3ca91ba0933fcfa8cbf
An Tran Van <86640630+antrvan746@users.noreply.github.com>
2023-07-13 23:46:41 +0700

    Test adding map to website

--- ride-hailing-webapp/components/admin/MapComponent.tsx
+++ ride-hailing-webapp/components/admin/MapComponent.tsx
@@ -1,7 +1,23 @@
-import React from "react";
 import Image from "next/image";
+import React, { Component } from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const ggMapApiKey = process.env.GGMAP_API_KEY || "AIzaSyDekTKGUSYNDS2O17iZV8Lw9l0ysEWtT_A";
+
+const containerStyle = {
+  width: '100%',
+  height: '100%'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 const MapComponent = () => {
+
+  console.log(ggMapApiKey)
+
   return (
     <div
       style={{
@@ -9,18 +25,19 @@ const MapComponent = () => {
         listStyle: "none",
         transition: "none",
         overflow: "hidden",
-        height: "98vh"
+        height: "98vh",
       }}
     >
-      <div
-        id="display-google-map"
-        style={{ height: "100%", width: "100%", maxWidth: "100%" }}
-      >
-        <iframe
-          style={{ height: "100%", width: "100%", border: 0 }}
-          src="https://www.google.com/maps/embed/v1/place?q=Nhà+thờ+Đức+Bà,+Công+xã+Paris,+Bến+Nghé,+Quận+1,+Thành+phố+Hồ+Chí+Minh,+Việt+Nam&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-        ></iframe>
-      </div>
+      <LoadScript googleMapsApiKey={ggMapApiKey || ""}>
+      <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        >
+          { /* Child components, such as markers, info windows, etc. */ }
+          <></>
+        </GoogleMap>
+      </LoadScript>
     </div>
   );
 };
